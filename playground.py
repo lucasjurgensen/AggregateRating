2d6b2f20423f0e0c32bb08a3713fbe16e1b3a461
Lucas Jurgensen <jurgensenlucas@gmail.com>
2020-11-11 00:12:55 -0500

    messed with getting reviews for a book by id

--- playground.py
+++ playground.py
@@ -1,13 +1,21 @@
 import urllib3
-import xml.etree.ElementTree as ET
+try:
+    # cElementTree is a faster, c-based version
+    import xml.etree.cElementTree as ET
+except ImportError:
+    # Resort to the standard, slower one if import fails
+    import xml.etree.ElementTree as ET
 
 # Grab developer key
 f = open("goodreads_key", "r")
 dev_key = f.read()
 f.close()
 
+############
+# Search for book example
 # Create URL
 search_param = "Ender%27s+Game"
+search_param = "The+Great+Gatsby"
 url = "https://www.goodreads.com/search.xml?key={}&q={}".format(dev_key, search_param)
 
 
@@ -18,6 +26,26 @@ string_xml = r.data
 root = ET.fromstring(string_xml)
 
 # Process Response
-for elem in root.iter("work"):
-    for elem1 in elem.iter("title"):
-        print(elem1.text)
+print("reached")
+for book in root.iter(tag="best_book"):
+    for id in book.iterfind("id"):
+        print(id.text)
+# for elem in root.iter("work"):
+#     for elem1 in elem.iter("title"):
+#         print(elem1.text)
+
+
+###############
+# Get reviews example
+book_id = 18079759
+url = "https://www.goodreads.com/book/show.xml?key={}&id={}".format(dev_key, book_id)
+
+# Get response
+http = urllib3.PoolManager()
+r = http.request('GET', url)
+string_xml = r.data
+print(string_xml)
+root = ET.fromstring(string_xml)
+
+# Process Response
+print(root)

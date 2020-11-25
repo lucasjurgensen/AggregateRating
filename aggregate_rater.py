44d5b53f9174582b5b9aa91cd01480178b413046
Lucas Jurgensen <jurgensenlucas@gmail.com>
2020-11-24 23:39:29 -0500

    (AR-1) Get reviews from a reviewer

--- aggregate_rater.py
+++ aggregate_rater.py
@@ -9,18 +9,28 @@ f.close()
 
 ##################
 # Definitions
-class book:
+
+class Book:
     title = ""
     id = ""
+
     def __init__(self, _title, _id):
         self.title = _title
         self.id = _id
 
-class review:
-    stars = -1
+    def __repr__(self):
+        return "{} - {}".format(self.title, self.id)
+
+class Review:
+    book = Book()
+    rating = -1
     text = ""
 
-class user:
+    __init__(self, _title, _book_id, _rating):
+        self.book = Book(_title, _book_id)
+        self.rating = _rating
+
+class User:
     name = ""
     id = ""
     reviews = []
@@ -28,21 +38,43 @@ class user:
 
 ##################
 # Select Book
-search_param = "Ender%27s+Game"
-url = "https://www.goodreads.com/search.xml?key={}&q={}".format(dev_key, search_param)
-http = urllib3.PoolManager()
-r = http.request('Get', url)
-string_xml = r.data
-root = ET.fromstring(string_xml)
+def get_book(search_param="Ender%27s+Game"):
+    url = "https://www.goodreads.com/search.xml?key={}&q={}".format(dev_key, search_param)
+    http = urllib3.PoolManager()
+    r = http.request('Get', url)
+    string_xml = r.data
+    root = ET.fromstring(string_xml)
 
-# Find the best book based on search
-best_book_found = root.find("search").find("results").find("work").find("best_book")
-book = book(_title=best_book_found.find("title").text, _id=best_book_found.find("id").text)
-    # break
+    # Find the best book based on search
+    best_book_found = root.find("search").find("results").find("work").find("best_book")
+    book = Book(_title=best_book_found.find("title").text, _id=best_book_found.find("id").text)
 
-print(book.title)
-print(book.id)
+    print(book)
+
+def get_user_bookshelf_reviews(user_id=120182276):
+    url = "https://www.goodreads.com/review/list?v=2&id={}&shelf={}&per_page={}&key={}".format(user_id,"read","200",dev_key)
+    http = urllib3.PoolManager()
+    r = http.request('GET', url)
+    string_xml = r.data
+    root = ET.fromstring(string_xml)
+    reviews = []
+    for review_full in root.find("reviews").findall("review"):
+        review = Review(_title=review_full.find("book").find("title").text,
+                        _id=review_full.find("book").find("id").text
+                        review_full.find("rating"))
+        rating = review.find("rating")
+        reviews.append(review)
+    return reviews
+
 
 
 ##################
 # Get User Reviews
+
+def main():
+    get_book()
+
+    get_reviews_for_book()
+
+if __name__ == "__main__":
+    main()
